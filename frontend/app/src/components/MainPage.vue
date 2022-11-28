a33192f9a67ff31eb48377b9d2c24ea933199aa6
Simone Baffelli <simone.baffelli@empa.ch>
2022-11-28 13:44:31 +0100

    Moved components related to chemicals to their domain

--- frontend/app/src/components/MainPage.vue
+++ frontend/app/src/components/MainPage.vue
@@ -4,7 +4,7 @@ import { ref, onBeforeMount, onMounted, getCurrentInstance, watch, markRaw, toRe
 import { storeToRefs, Store } from 'pinia';
 import { useMolecule } from '@/chemical/store/molecule.js/molecule';
 import { OpenbisObjectConfiguration, expandObject, reverseMapping, OpenbisCollection, OpenbsInstance, OpenbisObject } from '../openbis/model/utils.jsbis/model/utils'
-import ChemDraw from './ChemDraw.vue';
+import ChemDraw from '../chemical/components/ChemDraw.vue';
 import { useOpenbis } from "@/openbis/store/openbis";
 import ListView from './ListView.vue';
 import ListItemActions from "./ListItemActions.vue";
